822d783d553de0729be1ac66fd667a035267c1b4
xiahaifeng <xiahaifeng2000@gmail.com>
2023-12-18 23:04:30 +0800

    leetcode

--- src/test/java/com/xhf/leetCode/Solution20231218.java
+++ src/test/java/com/xhf/leetCode/Solution20231218.java
@@ -79,6 +79,38 @@ public class Solution20231218 {
         }
     }
 
+
+    /**
+     * leetCode 162. 寻找峰值
+     * 峰值元素是指其值严格大于左右相邻值的元素。给你一个整数数组 nums，
+     * 找到峰值元素并返回其索引。数组可能包含多个峰值，在这种情况下，
+     * 返回 任何一个峰值 所在位置即可。你可以假设 nums[-1] = nums[n] = -∞ 。
+     * 你必须实现时间复杂度为 O(log n) 的算法来解决此问题。
+     */
+    public int findPeakElement(int[] nums) {
+        if(nums.length==1){
+            return 0;
+        }
+        if(nums[0]>nums[1]){
+            return 0;
+        }
+        if(nums[nums.length-1]>nums[nums.length-2]){
+            return nums.length-1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        int middle = (left + right) / 2;
+        while (left < right) {
+            if (nums[middle] > nums[middle + 1]) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+            middle = (left + right) / 2;
+        }
+        return left;
+    }
+
     public static void main(String[] args) {
         Solution20231218 solution20231218= new Solution20231218();
         int[][] points = {{-17, -6}, {-4, 0}, {-2, -16}, {-1, 2}, {0, 11}, {6, 18}};
