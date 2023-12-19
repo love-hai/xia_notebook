d1b3eac4840a0908814f892f92afc159c7a46f66
夏海锋 <xiahaifeng2000@gmail.com>
2023-12-19 18:13:03 +0800

    leetcode

---
+++ src/test/java/com/xhf/leetCode/Solution20231219.java
@@ -0,0 +1,28 @@
+package com.xhf.leetCode;
+
+/**
+ * @projectName: test<br>
+ * @package: com.xhf.leetCode<br>
+ * @className: Solution20231219<br>
+ * @descriptions:
+ * @author: xiahaifeng
+ * @createDate: 2023/12/19 10:36
+ * @updateUser: xiahaifeng
+ * @updateDate: 2023/12/19 10:36
+ * @updateRemark:
+ */
+
+public class Solution20231219 {
+
+    /**
+     * leetCode 1901 寻找峰值
+     * 一个 2D 网格中的 峰值 是指那些 严格大于 其相邻格子(上、下、左、右)的元素。
+     * 给你一个 从 0 开始编号 的 m x n 矩阵 mat ，其中任意两个相邻格子的值都
+     * 不相同 。找出 任意一个 峰值 mat[i][j] 并 返回其位置 [i,j] 。你可以假
+     * 设整个矩阵周边环绕着一圈值为 -1 的格子。要求必须写出时间复杂度为 O(m log(n)) 或 O(n log(m)) 的算法
+     */
+    public int[] findPeakGrid(int[][] mat) {
+
+    }
+
+}
