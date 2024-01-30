115f14ffd334d795a17f6cc323b1108d36d8eb97
夏海锋 <xiahaifeng2000@gmail.com>
2024-01-30 08:42:34 +0800

    增加日志

--- src/main/java/com/ljheee/thrift/service/impl/netty/HelloWorldServiceImpl.java
+++ src/main/java/com/ljheee/thrift/service/impl/netty/HelloWorldServiceImpl.java
@@ -2,16 +2,19 @@ package com.ljheee.thrift.service.impl.netty;
 
 import com.ljheee.thrift.service.defn.netty.HelloWolrdService;
 import com.ljheee.thrift.service.defn.netty.UserInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.thrift.TException;
-
+@Slf4j
 public class HelloWorldServiceImpl implements HelloWolrdService.Iface {
 
     @Override
     public UserInfo getUserInfo(String username, String password) throws TException {
         UserInfo userInfo = new UserInfo();
+        log.info("假装查找数据库，返回用户信息");
         userInfo.setUsername(username);
         userInfo.setPassword(password);
         userInfo.setRemark("remark");
+        log.info("返回用户信息"+userInfo.toString());
         return userInfo;
     }
 
