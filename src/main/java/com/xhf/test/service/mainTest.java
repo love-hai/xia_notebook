a1cac4bbf719e79bd9cf7bd75cd4c6311304d94b
夏海锋 <xiahaifeng2000@gmail.com>
2024-02-16 16:08:07 +0800

    uml学习

--- src/main/java/com/xhf/test/service/mainTest.java
+++ src/main/java/com/xhf/test/service/mainTest.java
@@ -1,30 +1,19 @@
 package com.xhf.test.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 
-import java.util.Scanner;
+import java.util.List;
 
 @Slf4j
 public class mainTest {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("请输入文本（遇到换行符即结束）：");
-
-        // 使用 hasNextLine() 判断是否还有下一行输入
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            System.out.println("你输入的是：" + line);
-
-            // 如果输入为空行（只有换行符），则结束循环
-            if (line.isEmpty()) {
-                break;
-            }
+        List<String> a = null;
+        if(CollectionUtils.isNotEmpty(a)){
+            log.info("a is not empty");
+        }else {
+            log.info("a is empty");
         }
-
-        // 关闭 Scanner
-        scanner.close();
     }
-
 }
